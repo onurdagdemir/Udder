d1c08d96b58e649e970ef97ada356c483c9571e1
Onur DAĞDEMİR <onurdagdemir@gmail.com>
2023-03-08 13:11:13 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Onur Dağdemir on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
